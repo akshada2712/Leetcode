bd7b3279c2ad871718fc8a02a2b9f1f80d4ed337
akshada2712 <62982666+akshada2712@users.noreply.github.com>
2023-02-07 22:39:28 -0500

    Time: 169 ms (73.91%), Space: 15.5 MB (27.36%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        n = len(nums)
+        for i in nums:
+            if i not in hashmap:
+                hashmap[i] = 1
+            else:
+                hashmap[i] += 1
+                
+        for i in hashmap:
+            if hashmap[i] > math.floor(n/2):
+                return i
+        
