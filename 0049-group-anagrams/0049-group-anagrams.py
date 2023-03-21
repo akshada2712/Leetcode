20459134a206721a2b899229df2405fd82a8cd2e
akshada2712 <62982666+akshada2712@users.noreply.github.com>
2023-03-21 10:46:46 -0400

    Time: 100 ms (73.01%), Space: 17.8 MB (61.61%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hashmap = {}
+        
+        for i in strs:
+            key = "".join(sorted(i))
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(i)
+            
+        return hashmap.values()
