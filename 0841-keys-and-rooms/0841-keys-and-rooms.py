433864e2763fd023f8838d3a9cd1adf724dc5436
akshada2712 <62982666+akshada2712@users.noreply.github.com>
2023-06-04 18:00:13 -0400

    Time: 83 ms (45.35%), Space: 16.8 MB (46.74%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,9 +1,9 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         visit = set()
-        
+        # add first room to the queue
         q = deque([0])
-        
+        # we will chk the length of visit room == len(rooms)
         while q:
             curr = q.popleft()
             visit.add(curr)
