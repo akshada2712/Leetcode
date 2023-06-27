9d4c957fe17377171532d9effa24bd825f67b3e2
akshada2712 <62982666+akshada2712@users.noreply.github.com>
2023-06-26 23:11:57 -0400

    Time: 202 ms (51.08%), Space: 16.7 MB (48.44%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,22 +1,38 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         
-        def helper(i,j):
-            if i == len(nums1) or j == len(nums2):
-                return 0
-            
-            if (i,j) in dp:
-                return dp[(i,j)]
-            
-            if nums1[i] == nums2[j]:
-                dp[(i,j)] = 1 + helper(i+1, j+1)
-                
-            else:
-                dp[(i,j)] = max(helper(i+1,j), helper(i,j+1))
-            return dp[(i,j)]
+        # dynamic programming - matrix solution
         
-        dp = {}
-        return helper(0,0)
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1)+1)]
+        
+        
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    dp[i+1][j+1] = 1 + dp[i][j]
+                else:
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+                    
+        return dp[len(nums1)][len(nums2)]
+        
+#         # recursive memoization solution
+        
+#         def helper(i,j):
+#             if i == len(nums1) or j == len(nums2):
+#                 return 0
+            
+#             if (i,j) in dp:
+#                 return dp[(i,j)]
+            
+#             if nums1[i] == nums2[j]:
+#                 dp[(i,j)] = 1 + helper(i+1, j+1)
+                
+#             else:
+#                 dp[(i,j)] = max(helper(i+1,j), helper(i,j+1))
+#             return dp[(i,j)]
+        
+#         dp = {}
+#         return helper(0,0)
         
         
 #         hash1 = {}
