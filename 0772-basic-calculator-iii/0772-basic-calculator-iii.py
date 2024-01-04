4a2c0193d380d20bd81077e407e4fb6e901e0110
akshada2712 <62982666+akshada2712@users.noreply.github.com>
2024-01-04 13:47:39 +0530

    Time: 39 ms (88.27%), Space: 17.4 MB (16.63%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.py
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.py
@@ -1,105 +1,44 @@
-# class Solution:
-#     def calculate(self, s: str) -> int:
-        
-#         def evaluate(x, y, op):
-#             if op == "+":
-#                 return x
-#             if op == '-':
-#                 return -x 
-#             if op == '*':
-#                 return x * y 
-#             return math.trunc(x/y)
-#         s += '@'
-#         stack = []
-#         curr = 0
-#         prevOp = "+"
-        
-        
-#         for c in s:
-#             if c.isdigit():
-#                 curr = curr * 10 + int(c)
-                
-#             elif c == '(':
-#                 stack.append(prevOp)
-#                 prevOp ="+"
-                
-#             else:
-#                 if c in "*/":
-#                     stack.append(evaluate(stack.pop(), curr, prevOp))
-#                 else:
-#                     stack.append(evaluate(curr, 0, prevOp))
-                
-#                 curr = 0
-#                 prevOp = c
-#                 if c == ')':
-#                     while type(stack[-1] == int):
-#                         curr += stack.pop()
-#                     prevOp = stack.pop()
-                    
-#         return sum(stack)
-# #         stack = []
-# #         curr = 0
-# #         prevOp = "+"
-# #         s += "@"
-        
-# #         for i in s:
-# #             print(stack,i,prevOp)
-# #             if i.isdigit():
-# #                 curr = curr * 10 + int(i)
-# #             elif i == '(':
-# #                 stack.append(prevOp)
-# #                 prevOp = "+"
-# #             else:
-# #                 if prevOp == "+":
-# #                     stack.append(curr)
-# #                 elif prevOp == '-':
-# #                     stack.append(-curr)
-# #                 elif prevOp == '*':
-# #                     stack.append(stack.pop() * curr)
-# #                 elif prevOp == '/':
-# #                     stack.append(math.trunc(stack.pop() / curr))
-# #                 curr = 0
-# #                 prevOp = i 
-# #                 if i == ')':
-# #                     while type(stack[-1]) == int():
-# #                         curr += stack.pop()
-# #                     prevOp = stack.pop()
-# #         print(stack)           
-# #         return sum(stack)
-        
 class Solution:
     def calculate(self, s: str) -> int:
-        def evaluate(x, y, operator):
-            if operator == "+":
-                return x
-            if operator == "-":
-                return -x
-            if operator == "*":
-                return x * y
-            return int(x / y)
         
+        def evaluate(x, y, op):
+            if op == "+":
+                return x
+            if op == '-':
+                return -x 
+            if op == '*':
+                return x * y 
+            
+            return int(x/y)
+        
+        s += '@'
         stack = []
         curr = 0
-        previous_operator = "+"
-        s += "@"
+        prevOp = "+"
+        
         
         for c in s:
             if c.isdigit():
                 curr = curr * 10 + int(c)
-            elif c == "(":
-                stack.append(previous_operator)
-                previous_operator = "+"
+                
+            elif c == '(':
+                stack.append(prevOp)
+                prevOp ="+"
+                
             else:
-                if previous_operator in "*/":
-                    stack.append(evaluate(stack.pop(), curr, previous_operator))
+                if prevOp in "*/":
+                    stack.append(evaluate(stack.pop(), curr, prevOp))
                 else:
-                    stack.append(evaluate(curr, 0, previous_operator))
+                    stack.append(evaluate(curr, 0, prevOp))
                 
                 curr = 0
-                previous_operator = c
-                if c == ")":
+                prevOp = c
+                if c == ')':
                     while type(stack[-1]) == int:
                         curr += stack.pop()
-                    previous_operator = stack.pop()
-
+                    prevOp = stack.pop()
+                    
         return sum(stack)
+    
+    
+  
