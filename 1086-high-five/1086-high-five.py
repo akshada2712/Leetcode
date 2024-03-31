21de25e194f8c2dbd483bea5dd6ebb381ddba23b
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-03-31 15:46:06 -0400

    Time: 79 ms (11.24%), Space: 17.2 MB (12.29%) - LeetHub

--- 1086-high-five/1086-high-five.py
+++ 1086-high-five/1086-high-five.py
@@ -1,17 +1,41 @@
+import heapq
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
         results =  {}
         
-        for id, score in items:
+        # Using maxHeap 
+        
+        for id, scores in items:
             if id not in results:
                 results[id] = []
-            results[id].append(score)   
-            
+                heapq.heapify(results[id])
+                
+            heapq.heappush(results[id], scores)
+           
         avgScore = []
-        
-        for i, scores in results.items():
-            scores = sorted(scores, reverse = True)
-            avgScore.append([i, int(sum(scores[:5]) / 5)])
+        for i, score in results.items():
+            scores = heapq.nlargest(5, score)
+            avgScore.append([i, int(sum(scores) / 5)])
             
         return sorted(avgScore, key = lambda x:x[0])
+            
+                
+        
+#         for id, score in items:
+#             if id not in results:
+#                 results[id] = []
+#             results[id].append(score)   
+            
+#         avgScore = []
+        
+#         for i, scores in results.items():
+#             scores = sorted(scores, reverse = True)
+#             avgScore.append([i, int(sum(scores[:5]) / 5)])
+            
+#         return sorted(avgScore, key = lambda x:x[0])
+
+
+        
+    
+        
         
