1d4a6005da54652e2cd954d0a67f1370f6427e4e
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-03-31 21:16:55 -0400

    Time: 40 ms (24.42%), Space: 16.5 MB (54.10%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -1,22 +1,57 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         
-        dirX, dirY = 0, 1  # initial north
+        dirsmap = {"N": [0,1], "S":[0,-1], "E": [1,0], "W": [-1,0]}
         
-        x, y = 0, 0   # inital cordinates
-        
+        currDir = "N"
+        x, y = 0, 0
+        instructions = instructions * 4 # checking all 4 sides
         for i in instructions:
-            if i == "G":   # easy case, move one step ahead so we add diretion coordinates
-                x, y = x + dirX, y + dirY
+            if i == "G":
+                x, y = x + dirsmap[currDir][0], y + dirsmap[currDir][1]
+            elif i == 'L':
+                if currDir == "N":
+                    currDir = "W"
+                elif currDir == 'S':
+                    currDir = "E"
+                elif currDir == "E":
+                    currDir = "N"
+                else:
+                    currDir = "S"
+            elif i == 'R':
+                if currDir == "N":
+                    currDir = "E"
+                elif currDir == 'S':
+                    currDir = "W"
+                elif currDir == "E":
+                    currDir = "S"
+                else:
+                    currDir = "N"
+                    
+                    
+        return x == 0 and y == 0
                 
-            elif i == "L":   # moving to left, turning by 90 deg acc to linear algebra rotating is swapping values 
-                # multiply X by -1 as we want to go left side not right side
-                dirX, dirY = -1 * dirY, dirX   
+                    
+            
+
+
+        
+#         dirX, dirY = 0, 1  # initial north
+        
+#         x, y = 0, 0   # inital cordinates
+        
+#         for i in instructions:
+#             if i == "G":   # easy case, move one step ahead so we add diretion coordinates
+#                 x, y = x + dirX, y + dirY
                 
-            else:
-                dirX, dirY = dirY, -1 * dirX
+#             elif i == "L":   # moving to left, turning by 90 deg acc to linear algebra rotating is swapping values 
+#                 # multiply X by -1 as we want to go left side not right side
+#                 dirX, dirY = -1 * dirY, dirX   
+                
+#             else:
+#                 dirX, dirY = dirY, -1 * dirX
                  
-        return (x,y) == (0,0) or (dirX, dirY) != (0,1)   # we are checking that the cordinates should be original ones and directions should be not same
+#         return (x,y) == (0,0) or (dirX, dirY) != (0,1)   # we are checking that the cordinates should be original ones and directions should be not same
                 
         
 #         dirsmap = {"N": [0,1], "S":[0,-1], "E": [1,0], "W": [-1,0]}
