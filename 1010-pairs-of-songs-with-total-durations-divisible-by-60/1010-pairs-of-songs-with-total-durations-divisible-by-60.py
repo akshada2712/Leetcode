176b07d93fa4e946170ed39777d1486f89e3cffa
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-01 00:36:33 -0400

    Time: 226 ms (64.43%), Space: 20.9 MB (32.30%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,26 +1,18 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        hashmap = collections.defaultdict(int)
+        
+        # modified two sum 
+        
+        map = collections.defaultdict(int)
+        
         pairs = 0
         
         for t in time:
             if t % 60 == 0:
-                pairs += hashmap[0]
+                pairs += map[t%60]
             else:
-                pairs += hashmap[60 - t%60]
-            
-            hashmap[t%60] += 1
-            #print(t,hashmap)
+                pairs += map[60 - t%60]
+                
+            map[t%60] += 1 
             
         return pairs
-#         pairs = 0
-#         m = len(time)
-#         hashmap = set()
-#         for i in range(m):
-#             for j in range(i+1,m):
-#                 if (time[i] + time[j]) % 60 == 0 and (i,j) not in hashmap and (j,i) not in hashmap:
-#                     pairs += 1
-#                     hashmap.add((i,j))
-#         #print(hashmap)            
-#         return pairs
-        
