e7be4e024dbb2f932db5d544e1287baabb7a8e0d
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-03-31 21:11:10 -0400

    Time: 37 ms (45.95%), Space: 16.5 MB (54.10%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -3,19 +3,20 @@ class Solution:
         
         dirX, dirY = 0, 1  # initial north
         
-        x, y = 0, 0 
+        x, y = 0, 0   # inital cordinates
         
         for i in instructions:
-            if i == "G":
+            if i == "G":   # easy case, move one step ahead so we add diretion coordinates
                 x, y = x + dirX, y + dirY
                 
-            elif i == "L":
-                dirX, dirY = -1 * dirY, dirX
+            elif i == "L":   # moving to left, turning by 90 deg acc to linear algebra rotating is swapping values 
+                # multiply X by -1 as we want to go left side not right side
+                dirX, dirY = -1 * dirY, dirX   
                 
             else:
                 dirX, dirY = dirY, -1 * dirX
-                
-        return (x,y) == (0,0) or (dirX, dirY) != (0,1)
+                 
+        return (x,y) == (0,0) or (dirX, dirY) != (0,1)   # we are checking that the cordinates should be original ones and directions should be not same
                 
         
 #         dirsmap = {"N": [0,1], "S":[0,-1], "E": [1,0], "W": [-1,0]}
