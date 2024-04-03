0814d865ef956b4158e4a5a04ca07dc877231088
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-03 18:29:11 -0400

    Time: 344 ms (5.03%), Space: 34.7 MB (27.71%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,15 +1,37 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
+        # using cycle sort  -> give tle
+        
+#         i = 0
+#         n = len(nums) 
+        
+#         while i < n:
+#             curr = nums[i] - 1
+#             if nums[i] != nums[curr]:
+#                 nums[i], nums[curr] = nums[curr], nums[i]
+#             else:
+#                 i += 1 
+                
+#         for i in range(n):
+#             if nums[i] != i + 1:
+#                 return i + 1
+            
+#         return n + 1
+        
+    # create hashmap of all numbers in nums
         hash_ = set(nums)
         
+        # check if 1 is present as it is smallest posiitve integer
         if 1 not in hash_:
             return 1 
         
+        # run a loop for maxium element + 2  and check if number is present or not
         for i in range(max(nums) + 2):
             if i not in hash_ and i > 0:
                 return i 
-            
+        
+        # return length of numbers if everything is presemt
         return len(nums)
         
 #         minNum = min(nums)
