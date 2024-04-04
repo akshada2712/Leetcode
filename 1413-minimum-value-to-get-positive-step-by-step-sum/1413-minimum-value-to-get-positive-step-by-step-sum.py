ebacad539b6f52db9266198e6b57467326577973
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-04 18:08:24 -0400

    Time: 35 ms (69.10%), Space: 16.5 MB (38.45%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -2,12 +2,15 @@ class Solution:
     def minStartValue(self, nums: List[int]) -> int:
         min_val = 0
         tot = 0 
+        pref_sum = [0] * len(nums)
+        pref_sum[0] = 0
         
         for num in nums:
-            tot += num 
-            print(tot)
-            min_val = min(min_val, tot)
+            pref_sum.append(pref_sum[-1] + num)
+            # tot += num 
+            # print(tot)
+            # min_val = min(min_val, tot)
             
-        return -min_val + 1
+        return -min(pref_sum) + 1
             
         
