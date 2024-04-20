e378c5f04c0d22d736885a10944e32f0a3f6b834
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-20 11:15:17 -0400

    Time: 91 ms (5.66%), Space: 20.2 MB (30.74%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -2,28 +2,52 @@ import heapq
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        res = []
-        heapq.heapify(res)
-        if not intervals:
-            return 0
         
-        intervals.sort(key = lambda x:x[0])
-        heapq.heappush(res, intervals[0][1])
-        
-        for i in intervals[1:]:
-            if res[0] <= i[0]:
-                heapq.heappop(res)
-                
-            heapq.heappush(res, i[1])
+         # approach 2 - using two pointers Time - O(nlogn)  Space - O(n)
             
-        return len(res)
+        start = [x[0] for x in intervals]
+        end = [x[1] for x in intervals]
         
-#         for i in range(len(intervals)):
-#             if res and res[-1][1] <= intervals[i][0]:
-#                 res.pop(-1)
-#             res.append(intervals[i])
+        start.sort()
+        end.sort()
         
+        count = 0
+        
+        l = 0 
+        r = 0
+        res = 0
+        
+        while l < len(start):
+            print(l, r)
+            if start[l] < end[r]:
+                
+                count += 1
+                print(count)
+                l += 1
+            else:
+                r += 1
+                count -= 1
+            res = max(res, count)  
+        #print(count)
+        return res
+        
+        # approach 1 - heapq
+#         res = []
+#         heapq.heapify(res)
+#         if not intervals:
+#             return 0
+        
+#         intervals.sort(key = lambda x:x[0])
+#         heapq.heappush(res, intervals[0][1])
+        
+#         for i in intervals[1:]:
+#             if res[0] <= i[0]:
+#                 heapq.heappop(res)
+                
+#             heapq.heappush(res, i[1])
+            
 #         return len(res)
+        
                     
                 
             
