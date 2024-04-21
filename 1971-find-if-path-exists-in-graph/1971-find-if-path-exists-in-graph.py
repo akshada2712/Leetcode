34a312ab7ee5e1d9c58942d2a4bfa8793a2ef83b
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-20 20:19:08 -0400

    Time: 1418 ms (85.39%), Space: 108.8 MB (58.93%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,8 +1,8 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        # use dfs 
-        # create a graph 
+        
+        # approach 2 -using bfs
         
         graph = collections.defaultdict(list)
         
@@ -12,24 +12,55 @@ class Solution:
             graph[u].append(v)
             graph[v].append(u)
             
+        q = collections.deque()
         seen = set()
-        #print(graph)
-        def dfs(src):
-           
-            if src == destination:
+        q.append(source)
+        seen.add(source)
+        
+        while q:
+            curr = q.popleft()
+            if curr == destination:
                 return True
             
-            if src not in seen:
-                seen.add(src)
-                for nei in graph[src]:
-                    if dfs(nei):
-                        return True
-                    
-            return False
+            for nei in graph[curr]:
+                if nei not in seen:
+                    seen.add(nei)
+                    q.append(nei)
+        return False
         
-        return dfs(source)
+        
+        
+        # approach 1 - using dfs
+        
+#         # use dfs 
+#         # create a graph 
+        
+#         graph = collections.defaultdict(list)
+        
+        
+#         # as it is bidirectional
+#         for u,v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
+            
+#         seen = set()
+#         #print(graph)
+#         def dfs(src):
+           
+#             if src == destination:
+#                 return True
+            
+#             if src not in seen:
+#                 seen.add(src)
+#                 for nei in graph[src]:
+#                     if dfs(nei):
+#                         return True
+                    
+#             return False
+        
+#         return dfs(source)
            
             
                 
-        dfs(source)
+#         dfs(source)
         
