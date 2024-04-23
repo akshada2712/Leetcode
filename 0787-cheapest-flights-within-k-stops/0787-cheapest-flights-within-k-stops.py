f55d0825c2ba8326a17e9e0769bfc41d384332ab
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-23 18:27:25 -0400

    Time: 187 ms (16.70%), Space: 21.9 MB (5.32%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,34 +1,34 @@
-import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        dist = [float(inf)] * n
-        pq = []
         
-        heappush(pq,(0,0,src))
+        graph = collections.defaultdict(list)
+        
+        for u,v,p in flights:
+            graph[u].append((v,p))
+            
+        prices = [(0, src, 0)]
+        heapq.heapify(prices)
         visit = set()
-       
-        
-        graph = defaultdict(list)
-        
-        for a,b,c in flights:
-            graph[a].append((b,c))
+        while prices:
+            cost, node, stops = heapq.heappop(prices)
             
-        while pq:
-            currCost, hops, node = heappop(pq)
             if node == dst:
-                return currCost
+                return cost 
             
-            if (node,currCost) in visit or hops > k:
-                continue
-                            
-            visit.add((node, currCost))
-            for nei,cost in graph[node]:
-                heappush(pq,(currCost + cost,hops+1,nei))
+            if (node, cost) in visit or stops > k:
+                continue 
+            
+            visit.add((node, cost))
+            
+            for newNode, newCost in graph[node]:
+                heapq.heappush(prices, (newCost+cost, newNode, stops + 1))
                 
-            
+        #print(cost)
+        
         return -1
-                
-                
         
         
+                
+            
+            
         
