28c4f3f5aec49dfc025b4a8ff2353498428db1d6
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-04-23 17:05:08 -0400

    Time: 80 ms (96.17%), Space: 18.4 MB (44.86%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,42 +1,32 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
-        # we will use Kahn's Algo -> as there is dependencies
-        
-        graph = defaultdict(list)
-        
-        for i, j in prerequisites:
-            graph[j].append(i)
+        graph = {i:[] for i in range(numCourses)}
+        for u, v in prerequisites:
+            graph[u].append(v)
             
-        in_deg = [0] * numCourses
+        path, visit = set(), set()
+        output = []
         
+        def dfs(src):
+            if src in path:
+                return False
+            
+            if src in visit:
+                return True
+            
+            path.add(src)
+            
+            for nei in graph[src]:
+                if dfs(nei) == False:
+                    return False
+            path.remove(src)
+            visit.add(src)
+            output.append(src)
+            return True 
+      
         for i in range(numCourses):
-            for j in graph[i]:
-                in_deg[j] = in_deg[j] + 1
-                
-        # print(in_deg, graph)
-        
-        q = deque()   # queue to hold course with 0 prereq
-        
-        for i in range(numCourses):
-            if in_deg[i] == 0:
-                q.append(i)
-            
-        idx = 0
-        order = [0] * numCourses
-        
-        while q:
-            node = q.popleft()
-            
-            order[idx] = node
-            idx += 1
-            for nei in graph[node]:
-                in_deg[nei] -= 1
-                if in_deg[nei] == 0:
-                    q.append(nei)
-                    
-        return order if idx == numCourses else []
-            
-            
-            
+            if dfs(i) == False:
+                return []
+     
+        return output
         
