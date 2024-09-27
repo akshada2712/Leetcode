1ddf622ee30750e7fd83f483620ee3d20e2df5e0
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-09-26 22:26:42 -0400

    Time: 38 ms (46.16%), Space: 16.6 MB (48.80%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -1,9 +1,14 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        
+        # use counter -> if the character is absent it will return 0 
         c_w1 = Counter(word1)
         c_w2 = Counter(word2)
+        
+        # we create a set which is union of all characters from both words, easy to iterate and handle edges cases for character not present in either of the words
         common = set(word1 + word2)
         for char in common:
+            
             if abs(c_w1[char] - c_w2[char]) > 3:
                 return False
             
