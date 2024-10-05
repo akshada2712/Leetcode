458b0e1c337ac18b32d4b7ed69af15894e23570e
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-10-04 20:07:59 -0400

    Time: 79 ms (18.44%), Space: 19.8 MB (60.71%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,7 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort()
+        intervals.sort()  # first sort 
+        # loop through to check overlaps 
         for i in range(len(intervals) - 1):
+            # compare the second element with the first element of next interval eg : 
+            # [0,30], [5,10] -> 30 >  5 -> False 
+            # [2,4], [7, 10] -> 4 is not > 7 and list ends hence True
+            
             if intervals[i][1] > intervals[i+1][0]:
                 return False 
             
