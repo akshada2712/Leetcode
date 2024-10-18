81382aeffd88564006983aa892f03a9a643a7f3b
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-10-17 22:29:00 -0400

    Time: 68 ms (100.00%), Space: 78.5 MB (33.03%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,41 +2,29 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.dic = collections.OrderedDict()
         
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.dic:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        
+        self.dic.move_to_end(key)
+        return self.dic[key]
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-            
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(False)
-#         self.key = key
-#         self.value = value
-#         if key in self.cache:
-#             self.cache.update(key, value)
-            
-#         self.cache[key] = value
-#         print(self.cache)
-#         if len(self.cache) > self.capacity:
-#             self.cache.popitem()
+        
+        if key in self.dic:
+            self.dic.move_to_end(key)
+        self.dic[key] = value
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(False)
             
         
-#         if len(self.cache) < self.capacity:
-#             self.cache[key] = value
-#         else:
-#             print(self.cache)
-#             self.cache.popitem()
-#             self.cache[key] = value
             
-#         return self.cache
+        
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
