02cbdc23fee9a73e58cfda8c7fcfed768451af81
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-10-24 08:13:16 -0400

    Time: 156 ms (94.80%), Space: 77.4 MB (91.33%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,25 +1,71 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None 
+    
+    
 class LRUCache:
-
+    
+    
+    # WE will use DLL 
+    
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.dic = collections.OrderedDict()
+        self.cap = capacity 
+        self.cache = {}   # dictionary to store node pointers 
         
-
-    def get(self, key: int) -> int:
-        if key not in self.dic:
-            return -1
+        self.left, self.right = Node(0, 0), Node(0,0)   #initialize left and right pointers for insertion
+        self.left.next, self.right.prev = self.right, self.left # connecting left and right l -> <- r
         
-        self.dic.move_to_end(key)
-        return self.dic[key]
+    def remove(self, node):
+        prev, nxt = node.prev, node.next 
+        prev.next, nxt.prev = nxt, prev 
+    
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right 
+        prev.next = nxt.prev = node 
+        node.next, node.prev = nxt, prev 
+    
+    def get(self, key: int) -> int: 
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val 
         
-
+        return -1 
+    
     def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
         
-        if key in self.dic:
-            self.dic.move_to_end(key)
-        self.dic[key] = value
-        if len(self.dic) > self.capacity:
-            self.dic.popitem(False)
+        if len(self.cache) > self.cap:
+            lru = self.left.next 
+            self.remove(lru)   # removing first node
+            del self.cache[lru.key]
+        
+    
+    
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.dic = collections.OrderedDict()
+        
+
+#     def get(self, key: int) -> int:
+#         if key not in self.dic:
+#             return -1
+        
+#         self.dic.move_to_end(key)
+#         return self.dic[key]
+        
+
+#     def put(self, key: int, value: int) -> None:
+        
+#         if key in self.dic:
+#             self.dic.move_to_end(key)
+#         self.dic[key] = value
+#         if len(self.dic) > self.capacity:
+#             self.dic.popitem(False)
             
         
             
