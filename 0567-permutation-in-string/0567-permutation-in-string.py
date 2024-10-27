a2e3548abafe3d396f6417f789588ed740fad913
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-10-27 12:08:27 -0400

    Time: 1077 ms (18.11%), Space: 16.9 MB (7.71%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,38 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+        count_s1 = Counter(s1)
         
-        l = 0 
-        r = 0 
-        n = len(s1)
-        counter = {}
-        hashmap = Counter(s1)
+        window_size = len(s1)
         
-        while r < len(s2):
-            #print(l,r, counter, hashmap)
-            while r - l + 1 <= n:
-                #print(s2[r])
-                if s2[r] not in counter:
-                    #print(s2[r], counter)
-                    counter[s2[r]] = 1
-                else:
-                    counter[s2[r]] += 1
-                r += 1
-                
-            if counter == hashmap:
-                return True
-            else:
-                counter[s2[l]] -= 1
-                if counter[s2[l]] == 0:
-                    del counter[s2[l]]
-                    
-                l += 1
-                
-        return False
-                
-                
-                
+        l = 0
+        r = window_size + l
+        
+        while r <= len(s2):
+            print(l,r)
+            if Counter(s2[l:r]) == count_s1:
+                return True 
             
+            l += 1
+            r += 1
+            
+        return False
             
         
