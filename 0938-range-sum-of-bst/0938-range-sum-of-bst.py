99fdb050deae6071522ee2ba9df7a24834a35b71
Akshada Bhandari <62982666+akshada2712@users.noreply.github.com>
2024-12-16 21:05:12 -0500

    Time: 19 ms (23.32%), Space: 25 MB (7.83%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -6,29 +6,50 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        values = []
         
+        # 2nd method 
+        
+        currSum = 0 
         
         def dfs(node):
+            nonlocal currSum
             if not node:
-                return 
+                return 0
             
+            if node.val >= low and node.val <= high:
+                currSum += node.val 
+                
             dfs(node.left)
-            values.append(node.val)
             dfs(node.right)
+            
+            return currSum
         
-        dfs(root)
+        return dfs(root)
         
-        ans = 0
-        for i in range(len(values)):
-            if values[i] == low:
-                while values[i] != high: 
-                    ans += values[i]
-                    i += 1
+        # 1st method
+#         values = []
+        
+        
+#         def dfs(node):
+#             if not node:
+#                 return 
+            
+#             dfs(node.left)
+#             values.append(node.val)
+#             dfs(node.right)
+        
+#         dfs(root)
+        
+#         ans = 0
+#         for i in range(len(values)):
+#             if values[i] == low:
+#                 while values[i] != high: 
+#                     ans += values[i]
+#                     i += 1
                     
-                ans += high 
+#                 ans += high 
                 
-                break 
+#                 break 
                 
-        return ans
+#         return ans
             
